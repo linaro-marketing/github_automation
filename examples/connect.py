3b3a27e7cfd5c68d9b91a8d849e4c704ca28570b
Kyle Kirkby <kyle.kirkby@linaro.org>
2020-02-17 09:29:23 +0000

    Added python code to simulate being in the base of the project directory

--- examples/connect.py
+++ examples/connect.py
@@ -1,5 +1,9 @@
-import GitHubManager from github_automation
+import sys
+import os
 
+sys.path.insert(0, os.path.abspath(
+    os.path.join(os.path.dirname(__file__), '..')))
+import GitHubManager from github_automation
 
 if __name__ == "__main__":
     ghm = GitHubManager()

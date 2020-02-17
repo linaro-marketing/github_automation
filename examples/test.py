f44eabb43c270a661d491d052b796e201635a563
Kyle Kirkby <kyle.kirkby@linaro.org>
2020-02-17 09:52:13 +0000

    Renamed the test file to be more generic

--- examples/test.py
+++ examples/test.py
@@ -3,7 +3,8 @@ import os
 
 sys.path.insert(0, os.path.abspath(
     os.path.join(os.path.dirname(__file__), '..')))
-import GitHubManager from github_automation
+
+from github_automation import GitHubManager
 
 if __name__ == "__main__":
     ghm = GitHubManager()
